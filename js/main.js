a24da7a2f6dfee4e2940331bdc290bbe1f0d3035
reehoiyan <re.hoiyan@gmail.com>
2016-08-08 12:38:45 +0800

    change empty css, make it opacity more slowly

--- js/main.js
+++ js/main.js
@@ -1,28 +1,43 @@
-window.onload = function(){
-  var right = document.getElementsByClassName('right'),
+var right = document.getElementsByClassName('right'),
   	  left =  document.getElementsByClassName('left');
 
+ window.onload = function(){
+  
+
   for(var i =0; i<3; i++){
-  	
-  	var h2 = right[i].getElementsByTagName('h2')[0],
-  	    div = right[i].getElementsByTagName('div')[0],
-  	    gp = left[i].getElementsByClassName('img_gp')[0],
+
+  	var gp = left[i].getElementsByClassName('img_gp')[0],
   	    mobile = left[i].getElementsByClassName('mobile')[0],
   	    works = gp.getElementsByTagName('img'),
   	    phone = mobile.getElementsByTagName('img');
 
   	    for(var w = 0; w < works.length; w++){
-  	    	works[w].src = works[w].getAttribute('data-src');
+  	    	works[w].onload = function(){
+				if(w === works.length){
+					for(var t = 0; t < 3; t++){
+						var div = right[t].getElementsByTagName('div')[0];
+	  						fadeIn(div);
+							div.innerHTML = div.getAttribute('data-content');
+					}	
+				}
+  	    	}
+  	    	fadeIn(works[w]);
+  	    	works[w].setAttribute('src', works[w].getAttribute('data-src'));
   	    }
   	    for(var p = 0; p < phone.length; p++){
-  	    	phone[p].src = phone[p].getAttribute('data-src');
+  	    	phone[p].onload = function(){
+  	    		if(p === phone.length){
+					for(var t = 0; t < 3; t++){
+						var h2 = right[t].getElementsByTagName('h2')[0];
+							fadeIn(h2);
+  	    					h2.innerHTML = h2.getAttribute('data-title');
+					}
+  	    		}
+  	    	}
+  	    	fadeIn(phone[p]);
+  	    	phone[p].setAttribute('src', phone[p].getAttribute('data-src'));
   	    }
-
-  	    fadeIn(gp);
-  	    fadeIn(mobile);
-  	    fadeIn(right[i]);
-  	    h2.innerHTML = h2.getAttribute('data-title');
-  	    div.innerHTML = div.getAttribute('data-content');
+  	    
   }
   
 }
@@ -31,7 +46,7 @@ function fadeIn(el){
 	el.style.opacity = 0;
   	(function fade() {
   	   var val = parseFloat(el.style.opacity);
-	   if ((val += .1) <= 1){
+	   if ((val += .01) <= 1){
 	  	    el.style.opacity = val;
 	  	    requestAnimationFrame(fade);
 	  	}

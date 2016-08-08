ed3d8e3e02782f66afb203a1985f0d2dd30f72f0
reehoiyan <re.hoiyan@gmail.com>
2016-08-08 13:03:59 +0800

    change the loaded content time

--- js/main.js
+++ js/main.js
@@ -1,52 +1,47 @@
 var right = document.getElementsByClassName('right'),
-  	  left =  document.getElementsByClassName('left');
+  	left =  document.getElementsByClassName('left');
 
  window.onload = function(){
   
 
   for(var i =0; i<3; i++){
 
-  	var gp = left[i].getElementsByClassName('img_gp')[0],
+  	var div = right[i].getElementsByTagName('div')[0],
+  		h2 = right[i].getElementsByTagName('h2')[0],
+  		gp = left[i].getElementsByClassName('img_gp')[0],
   	    mobile = left[i].getElementsByClassName('mobile')[0],
   	    works = gp.getElementsByTagName('img'),
   	    phone = mobile.getElementsByTagName('img');
 
   	    for(var w = 0; w < works.length; w++){
   	    	works[w].onload = function(){
-				if(w === works.length){
-					for(var t = 0; t < 3; t++){
-						var div = right[t].getElementsByTagName('div')[0];
-	  						fadeIn(div);
-							div.innerHTML = div.getAttribute('data-content');
-					}	
-				}
   	    	}
   	    	fadeIn(works[w]);
   	    	works[w].setAttribute('src', works[w].getAttribute('data-src'));
   	    }
   	    for(var p = 0; p < phone.length; p++){
   	    	phone[p].onload = function(){
-  	    		if(p === phone.length){
-					for(var t = 0; t < 3; t++){
-						var h2 = right[t].getElementsByTagName('h2')[0];
-							fadeIn(h2);
-  	    					h2.innerHTML = h2.getAttribute('data-title');
-					}
-  	    		}
   	    	}
   	    	fadeIn(phone[p]);
   	    	phone[p].setAttribute('src', phone[p].getAttribute('data-src'));
   	    }
-  	    
+	  	setTime(h2,div);
   }
   
 }
-
+function setTime(h2, div){
+	setTimeout(function(){
+		fadeIn(h2);
+		fadeIn(div);
+		h2.innerHTML = h2.getAttribute('data-title');
+		div.innerHTML = div.getAttribute('data-content');
+	}, 100);
+}
 function fadeIn(el){
 	el.style.opacity = 0;
   	(function fade() {
   	   var val = parseFloat(el.style.opacity);
-	   if ((val += .01) <= 1){
+	   if ((val += .1) <= 1){
 	  	    el.style.opacity = val;
 	  	    requestAnimationFrame(fade);
 	  	}

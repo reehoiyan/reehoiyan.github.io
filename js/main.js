7b340b8f7a7b1bea07d2fad938e1024771ec9137
reehoiyan <re.hoiyan@gmail.com>
2016-08-08 13:36:13 +0800

    refact the main.js

--- js/main.js
+++ js/main.js
@@ -1,58 +1,36 @@
-var right = document.getElementsByClassName('right'),
-  	left =  document.getElementsByClassName('left');
-
- window.onload = function(){
+window.onload = function(){
   
-
-  for(var i =0; i<3; i++){
-
-  	var gp = left[i].getElementsByClassName('img_gp')[0],
-  	    mobile = left[i].getElementsByClassName('mobile')[0],
-  	    works = gp.getElementsByTagName('img'),
-  	    phone = mobile.getElementsByTagName('img');
-
-  	    for(var w = 0; w < works.length; w++){
-  	    	works[w].onload = function(){
-  	    		if(w === works.length){
-  	    			start();
-  	    		}
-  	    	}
-  	    	works[w].setAttribute('src', works[w].getAttribute('data-src'));
-  	    }
-  	    for(var p = 0; p < phone.length; p++){
-  	    	phone[p].onload = function(){
-  	    	}
-  	    	phone[p].setAttribute('src', phone[p].getAttribute('data-src'));
-  	    }
-  }
-  
+ 	var img = document.getElementsByTagName('img'),
+ 		count = 0;
+ 		for(var j = 0; j < img.length; j++){
+ 			img[j].onload = function(){
+ 				count++;
+ 				start(count, img.length);
+ 			}
+ 			img[j].src = img[j].getAttribute('data-src');
+ 		}
 }
-function start(){
-	 for(var i =0; i<3; i++){
+function start(count, max){
+	
+	if(count !== max) return false;
+	var right = document.getElementsByClassName('right');
 
-  	var div = right[i].getElementsByTagName('div')[0],
-  		h2 = right[i].getElementsByTagName('h2')[0],
-  		gp = left[i].getElementsByClassName('img_gp')[0],
-  	    mobile = left[i].getElementsByClassName('mobile')[0],
-  	    works = gp.getElementsByTagName('img'),
-  	    phone = mobile.getElementsByTagName('img');
-  	    for(var w = 0; w < works.length; w++){
-  	    	fadeIn(works[w]);
-  	    }
-  	    for(var p = 0; p < phone.length; p++){
-  	    	fadeIn(phone[p]);
-  	    }
-  	    setTime(h2,div);
-  	  }
+	for(var r =0; r<right.length; r++){
+		var div = right[r].getElementsByTagName('div')[0],
+	  		h2 = right[r].getElementsByTagName('h2')[0],
+	  		img = document.getElementsByTagName('img');
+
+	  	    for(var i = 0; i < img.length; i++){
+	  	    	fadeIn(img[i]);
+	  	    }
+
+	  	    fadeIn(h2);
+	  	    fadeIn(div);
+	  	    h2.innerHTML = h2.getAttribute('data-title');
+			div.innerHTML = div.getAttribute('data-content');
+  	}
 }
-function setTime(h2, div){
-	setTimeout(function(){
-		fadeIn(h2);
-		fadeIn(div);
-		h2.innerHTML = h2.getAttribute('data-title');
-		div.innerHTML = div.getAttribute('data-content');
-	}, 100);
-}
+
 function fadeIn(el){
 	el.style.opacity = 0;
   	(function fade() {
